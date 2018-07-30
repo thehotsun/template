280c4fbd59d97e6ac94117a7fa1242ce2579ac89
thehotsun <3239601320@qq.com>
2018-07-30 15:30:19 +0800

    template测试

---
+++ template/src/components/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    this is my house!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
